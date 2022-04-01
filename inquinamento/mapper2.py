1a6e9269f8ea7ad51431c345017a3773473602f8
gabrielecosta <gabrielenicolocosta.90@gmail.com>
2022-04-01 22:39:39 +0000

    massimo europeo

---
+++ inquinamento/mapper2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+for line in sys.stdin:
+	line = line.strip()
+	column = line.split(",")
+	city = column[1]
+	country = column[2]
+	year_2021 = column[3]
+	city1 = "".join(c for c in city if c.isalpha())
+	country1 = "".join(c for c in country if c.isalpha())
+	#year_2021_1 = "".join(c for c in year_2021 if c.isalpha())
+	Europe = ["Italy", "Poland", "Croatia", "Belgium", "Poland", "Serbia", "Germany", "France"]
+	if country1 in Europe:
+		print('{0}\t{1}\t{2}'.format(country1,city1,year_2021))
+
+	
---
+++ inquinamento/reducer2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys,os
+
+last_country = None
+max_city = None
+max_val = 0
+
+for line in sys.stdin:
+	#line = line.strip()
+	country, city, year_2021 = line.split('\t')
+	try:
+		if last_country and last_country != country:
+			print(f"{last_country}\t{max_city}\t{max_val}")
+			last_country = country
+			max_city = city
+			max_val = float(year_2021)
+		else:
+			last_country = country
+			if max_city == None:
+				max_city = city
+				max_val = float(year_2021)
+			elif (float(year_2021) > max_val):
+				max_val = float(year_2021)
+				max_city = city
+	except ValueError:
+		print("Errore con la linea")
+
+if last_country:
+	print("{0}\t{1}\t{2}".format(last_country, max_city, max_val))
